6f473123822f1fe5127cdace07b5bbe48c3f62ce
Jean Vasconcelos <jean.vasconcelos@betrybe.com>
2020-01-28 15:12:39 -0300

    implement entrypoint for cypress evaluator

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -l
+
+git clone https://github.com/$GITHUB_REPOSITORY-tests.git /project-tests
+rm -rf /project-tests/.git
+cp -r /project-tests/* .
+npm install
+node_modules/.bin/cypress run
+node /evaluator.js cypress/reports/mochawesome.json requirements_mapping.json result.json
+
+if [ $? != 0 ]; then
+  echo "Execution error"
+  exit 1
+fi
+
+echo ::set-output name=result::`cat result.json | base64 -w 0`
+echo ::set-output name=pr-number::$(echo "$GITHUB_REF" | awk -F / '{print $3}')
