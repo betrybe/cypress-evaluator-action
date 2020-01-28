000d22646393c723ba58806201ab1a7cf827e3b2
Jean Vasconcelos <jean.vasconcelos@betrybe.com>
2020-01-28 18:12:32 -0300

    use official cypress image

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
-# FROM cypress/browsers:node12.13.0-chrome78-ff70
-FROM node:12.14
+FROM cypress/browsers:node12.13.0-chrome78-ff70
+# FROM node:12.14
 
 COPY entrypoint.sh /entrypoint.sh
 COPY evaluator.js /evaluator.js
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@
 git clone https://github.com/$GITHUB_REPOSITORY-tests.git /project-tests
 rm -rf /project-tests/.git
 cp -r /project-tests/* .
-# npm install
-# node_modules/.bin/cypress run
 ls -lah
 ls -lah cypress/reports
+npm install
+node_modules/.bin/cypress run
 node /evaluator.js cypress/reports/mochawesome.json requirements_mapping.json result.json
 
 if [ $? != 0 ]; then
@@ -14,6 +14,7 @@ if [ $? != 0 ]; then
   exit 1
 fi
 
-echo "::set-output name=pr-number::$(echo "$GITHUB_REF" | awk -F / '{print $3}')"
-pwd
-echo "::set-output name=result::`cat result.json | base64 -w 0`"
+echo "::set-output name=pr-number::$(echo "$GITHUB_REF" | awk -F / '{print $3}'),name=result::`cat result.json | base64 -w 0`"
+
+# echo "::set-output name=pr-number::$(echo "$GITHUB_REF" | awk -F / '{print $3}')"
+# echo "::set-output name=result::`cat result.json | base64 -w 0`"
