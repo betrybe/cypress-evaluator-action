081de6ab758eed641f9fd5a91b048125a8512676
Jean Paulo Silva Vasconcelos <jean.vasconcelos6@gmail.com>
2020-06-03 22:47:58 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,20 +4,20 @@ set -x
 REPOSITORY_NAME=$1
 REPOSITORY_BRANCH=$2
 
-# git clone --branch $REPOSITORY_BRANCH https://github.com/$REPOSITORY_NAME.git /project-tests
-# rm -rf /project-tests/.git
-# cp -r /project-tests/* .
+git clone --branch $REPOSITORY_BRANCH https://github.com/$REPOSITORY_NAME.git /project-tests
+rm -rf /project-tests/.git
+cp -r /project-tests/* .
 npm install
 npm install -g wait-on
 npm start & wait-on http://localhost:3000
 node_modules/.bin/cypress run
 ls
-# node /evaluator.js cypress/reports/mochawesome.json .trybe/requirements.json result.json
+node /evaluator.js cypress/reports/mochawesome.json .trybe/requirements.json result.json
 
 if [ $? != 0 ]; then
   echo "Execution error"
   exit 1
 fi
 
-# echo "::set-output name=pr-number::$(echo "$GITHUB_REF" | awk -F / '{print $3}')"
-# echo "::set-output name=result::`cat result.json | base64 -w 0`"
+echo "::set-output name=pr-number::$(echo "$GITHUB_REF" | awk -F / '{print $3}')"
+echo "::set-output name=result::`cat result.json | base64 -w 0`"
