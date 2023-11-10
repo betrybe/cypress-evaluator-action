48be6adab9e69e57cad5665ed00938b3691f003f
Renato Freire <renato.freire@betrybe.com>
2023-11-09 23:34:31 -0300

    Teste de execução

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,45 +1,47 @@
 #!/bin/bash
-set -x
+# set -x
 
-RUN_NPM_START=$1
-CYPRESS_HEADLESS_T=$2
-CYPRESS_BROWSER_T=$3
-RUN_JSON_SERVER=$4
-JSON_SERVER_PORT=$5
-JSON_SERVER_DB=$6
+echo "Running entrypoint.sh"
 
-export CY_CLI=true
+# RUN_NPM_START=$1
+# CYPRESS_HEADLESS_T=$2
+# CYPRESS_BROWSER_T=$3
+# RUN_JSON_SERVER=$4
+# JSON_SERVER_PORT=$5
+# JSON_SERVER_DB=$6
 
-npm install
+# export CY_CLI=true
 
-if $RUN_NPM_START ; then
-  npm start & # Open server in background
-  npx wait-on -t 300000 $wait_for_url # wait for server until timeout
-fi
+# npm install
 
-npx json-server --watch json-server/db.json --port 3050
+# if $RUN_NPM_START ; then
+#   npm start & # Open server in background
+#   npx wait-on -t 300000 $wait_for_url # wait for server until timeout
+# fi
+
+# npx json-server --watch json-server/db.json --port 3050
 
 
-if $RUN_JSON_SERVER ; then
-  npx json-server --watch json-server/$JSON_SERVER_DB --port $JSON_SERVER_PORT &
-  npx wait-on -t 300000 http://localhost:$JSON_SERVER_PORT
-fi
+# if $RUN_JSON_SERVER ; then
+#   npx json-server --watch json-server/$JSON_SERVER_DB --port $JSON_SERVER_PORT &
+#   npx wait-on -t 300000 http://localhost:$JSON_SERVER_PORT
+# fi
 
-headless_flag=''
-if $CYPRESS_HEADLESS_T ; then
-  headless_flag="--headless"
-fi
+# headless_flag=''
+# if $CYPRESS_HEADLESS_T ; then
+#   headless_flag="--headless"
+# fi
 
-node_modules/.bin/cypress install
-node_modules/.bin/cypress run "$headless_flag" --browser "$CYPRESS_BROWSER_T"
-ls
-node_modules/.bin/mochawesome-merge cypress/reports/*.json > output.json
-ls
-node /evaluator.js output.json .trybe/requirements.json result.json
+# node_modules/.bin/cypress install
+# node_modules/.bin/cypress run "$headless_flag" --browser "$CYPRESS_BROWSER_T"
+# ls
+# node_modules/.bin/mochawesome-merge cypress/reports/*.json > output.json
+# ls
+# node /evaluator.js output.json .trybe/requirements.json result.json
 
-if [ $? != 0 ]; then
-  echo "Execution error"
-  exit 1
-fi
+# if [ $? != 0 ]; then
+#   echo "Execution error"
+#   exit 1
+# fi
 
-echo "result=`cat result.json | base64 -w 0`" >> $GITHUB_OUTPUT
+# echo "result=`cat result.json | base64 -w 0`" >> $GITHUB_OUTPUT
